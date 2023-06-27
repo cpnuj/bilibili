a239fd875b97bd7561eaca66b9ba8fc6c8715db8
junp <junpc97@gmail.com>
2023-06-28 01:33:30 +0800

    prototype download and progress bar

--- bilibili.hs
+++ bilibili.hs
@@ -5,19 +5,14 @@
 module Main (main) where
 
 import Control.Applicative (empty)
-import System.IO ( stdout )
 import System.ProgressBar
-import Data.Text (Text)
-import Control.Monad.IO.Class (liftIO)
-import qualified Data.ByteString        as S
-import qualified Data.Conduit.List      as CL
+import GHC.Generics (Generic)
 import Conduit
 import Network.HTTP.Simple
-import GHC.Generics (Generic)
 import Data.Aeson
 import Data.ByteString (ByteString)
-import qualified Conduit as CL
--- import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString as S
+import Data.Conduit.Combinators (iterM)
 
 data Video = Video
   { videoId :: Int
@@ -40,45 +35,49 @@ instance FromJSON DashUrl where
   parseJSON (Object v) = Stuff <$> v .: "data"
   parseJSON _ = empty
 
-processed :: Int -> ByteString -> IO Int
-processed total input = do
-  let n = S.length input
-  return $ n + total
+progressStyle :: Style s
+progressStyle = Style
+  { styleOpen    = "["
+  , styleClose   = "]"
+  , styleDone    = '='
+  , styleCurrent = '>'
+  , styleTodo    = '.'
+  , stylePrefix  = msg "Bilibili"
+  , stylePostfix = percentage
+  , styleWidth   = ConstantWidth 80
+  , styleEscapeOpen    = const ""
+  , styleEscapeClose   = const ""
+  , styleEscapeDone    = const ""
+  , styleEscapeCurrent = const ""
+  , styleEscapeTodo    = const ""
+  , styleEscapePrefix  = const ""
+  , styleEscapePostfix = const ""
+  , styleOnComplete = WriteNewline
+  }
+
+showProgress :: IO (ProgressBar ()) -> ByteString -> IO (IO (ProgressBar ()))
+showProgress iop input = do
+  p <- iop
+  incProgress p (S.length input)
+  return $ return p
 
 main :: IO ()
 main = do
   response <- httpJSON "https://api.bilibili.com/x/player/playurl?bvid=BV1YW4y197Pe&cid=1162298090&fnval=16"
-  putStrLn $ "The status code was: " ++ show (getResponseStatusCode response)
-  print (getResponseHeader "Content-Type" response)
 
   let resp = (getResponseBody response :: DashUrl)
+  let url = (baseUrl . head . video . dash . datum) resp
 
-  let url = (baseUrl . head . video .dash . datum) resp
-
-  print url
-
-  request'  <- parseRequest url
+  request' <- parseRequest url
   let request = setRequestMethod "GET"
               $ setRequestHeader "Referer" ["https://www.bilibili.com"]
               $ setRequestHeader "User-Agent" ["Wget/1.21.3"]
               $ request'
 
-  -- response' <- httpLBS request
-  -- putStrLn $ "The status code was: " ++ show (getResponseStatusCode response')
-  -- print (getResponseHeader "Content-Type" response')
-  -- print (getResponseHeader "Content-Length" response')
-
-  httpSink request $ \r-> do
-      liftIO $ putStrLn
-             $ "The status code was: " ++ show (getResponseStatusCode r)
-
-      liftIO $ print (getResponseHeader "Content-Length" r)
-
-      -- let total = read $ show $ head $ getResponseHeader "Content-Length" r :: Int
-
-      -- let pb = newProgressBar defStyle 10 (Progress 0 total ())
-
-      -- lengthC
-      mapMC (S.hPut stdout)
+  httpSink request $ \r -> do
+    let total = read . filter ('"' /=) . show . head $ getResponseHeader "Content-Length" r :: Int
+    _ <- iterM (S.appendFile "video.m4s")
+      .| foldMC showProgress (newProgressBar progressStyle 10 (Progress 0 total ()))
+    sinkNull
 
   return ()
